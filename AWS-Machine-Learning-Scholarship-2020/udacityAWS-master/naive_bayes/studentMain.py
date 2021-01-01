52d5a56331a68d4f23eef7c83be3386618b80db3
shadab96 <shadab.cs0058@gmail.com>
2021-01-01 20:31:43 +0530

    AWS ML

---
+++ AWS-Machine-Learning-Scholarship-2020/udacityAWS-master/naive_bayes/studentMain.py
@@ -0,0 +1,24 @@
+import numpy as np
+import pylab as pl
+
+
+features_train, labels_train, features_test, labels_test = makeTerrainData()
+
+### the training data (features_train, labels_train) have both "fast" and "slow" points mixed
+### in together--separate them so we can give them different colors in the scatterplot,
+### and visually identify them
+grade_fast = [features_train[ii][0] for ii in range(0, len(features_train)) if labels_train[ii]==0]
+bumpy_fast = [features_train[ii][1] for ii in range(0, len(features_train)) if labels_train[ii]==0]
+grade_slow = [features_train[ii][0] for ii in range(0, len(features_train)) if labels_train[ii]==1]
+bumpy_slow = [features_train[ii][1] for ii in range(0, len(features_train)) if labels_train[ii]==1]
+
+
+# You will need to complete this function imported from the ClassifyNB script.
+# Be sure to change to that code tab to complete this quiz.
+clf = classify(features_train, labels_train)
+
+
+
+### draw the decision boundary with the text points overlaid
+prettyPicture(clf, features_test, labels_test)
+output_image("test.png", "png", open("test.png", "rb").read())
